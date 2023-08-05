25f874583dcc3a62eabbe71d92c4499236f0b4ea
Gabriel Felipe <gabrielfelipe1014@gmail.com>
2023-08-05 02:42:06 -0300

    Iniciando JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-alert('Olá!');
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+    })
+})
